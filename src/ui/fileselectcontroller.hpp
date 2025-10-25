d236bbe1e7f6bbfee295c7901ac4695df30cd566
darilin98 <darekrudis@email.cz>
2025-10-25 14:45:55 +0200

    Add file select window opening

--- src/ui/fileselectcontroller.hpp
+++ src/ui/fileselectcontroller.hpp
@@ -16,7 +16,33 @@ public:
 
     void valueChanged(VSTGUI::CControl* pControl) override
     {
-        fprintf(stderr, "Button called\n");
+        if (pControl->getValue() > 0.5) {
+            runFileSelector();
+        }
+    }
+private:
+    void runFileSelector() const {
+        auto* selector = VSTGUI::CNewFileSelector::create (nullptr,
+        VSTGUI::CNewFileSelector::kSelectFile);
+        if (selector)
+        {
+            selector->addFileExtension (VSTGUI::CFileExtension ("JPEG", "jpg"));
+            selector->addFileExtension (VSTGUI::CFileExtension ("PNG", "png"));
+            selector->addFileExtension (VSTGUI::CFileExtension ("GIF", "gif"));
+            selector->addFileExtension (VSTGUI::CFileExtension ("MOV", "mov"));
+            selector->addFileExtension (VSTGUI::CFileExtension ("AVI", "avi"));
+            selector->setDefaultExtension (VSTGUI::CFileExtension ("MP4", "mp4"));
+            selector->setTitle("[ Choose A Media File To Side Chain ]");
+            selector->run([this](VSTGUI::CNewFileSelector* sel)
+            {
+                if (sel->getNumSelectedFiles() > 0)
+                {
+                    const VSTGUI::UTF8String result = sel->getSelectedFile(0);
+                    fprintf(stderr, "Selected: %s\n", result.getString().c_str());
+                }
+                sel->forget();
+            });
+        }
     }
 };
 
