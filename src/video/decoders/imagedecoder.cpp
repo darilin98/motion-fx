3b9afcd592b3b374c246bc6cd63bd0675b537545
darilin98 <darekrudis@email.cz>
2025-11-08 12:41:28 +0100

    Implement simple image decoding

--- src/video/decoders/imagedecoder.cpp
+++ src/video/decoders/imagedecoder.cpp
@@ -1,5 +1,42 @@
 //
 // Created by Darek Rudiš on 08.11.2025.
 //
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 #include "imagedecoder.hpp"
+
+bool ImageDecoder::open(const std::string &path) {
+	int width, height, channels;
+
+	stbi_uc* stbi_data = stbi_load(path.c_str(), &width, &height, &channels, 4);
+	if (!stbi_data) {
+		return false;
+	}
+
+	cachedFrame.image.width = width;
+	cachedFrame.image.height = height;
+	cachedFrame.image.rgba_data.assign(
+		stbi_data,
+		stbi_data + width * height * 4
+	);
+	cachedFrame.timestamp = 0.0;
+	stbi_image_free(stbi_data);
+	loaded = true;
+	return true;
+}
+
+bool ImageDecoder::seekTo(int64_t time) {
+	// Does not make sense to call on image decoder
+	return loaded;
+}
+
+bool ImageDecoder::decodeNext(VideoFrame &outFrame) {
+	if (!loaded) {
+		return false;
+	}
+	outFrame = cachedFrame;
+	return true;
+}
+
+
--- src/video/decoders/imagedecoder.hpp
+++ src/video/decoders/imagedecoder.hpp
@@ -9,11 +9,16 @@
 
 class ImageDecoder final : public IMediaDecoder {
 public:
+	ImageDecoder() = default;
+	~ImageDecoder() override = default;
 	bool seekTo(int64_t time) override;
 	bool open(const std::string& path) override;
 	bool decodeNext(VideoFrame& outFrame) override;
 	[[nodiscard]] double frameRate() const override;
 	[[nodiscard]] bool isStillImage() const override;
+private:
+	VideoFrame cachedFrame;
+	bool loaded = false;
 };
 
 
