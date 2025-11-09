830ce1ba62b570bc4ae7555be4f1e667e20592ba
darilin98 <darekrudis@email.cz>
2025-11-09 19:04:31 +0100

    Add frame queue implementation

---
+++ src/video/framequeue.hpp
@@ -0,0 +1,45 @@
+//
+// Created by Darek Rudiš on 08.11.2025.
+//
+
+#ifndef FRAMEQUEUE_HPP
+#define FRAMEQUEUE_HPP
+
+#include <mutex>
+#include <queue>
+
+#include "videoframe.hpp"
+
+class IFrameQueue {
+public:
+	virtual ~IFrameQueue() = default;
+	virtual bool tryPop(VideoFrame& outFrame) = 0;
+	virtual void push(VideoFrame&& frame) = 0;
+
+};
+
+class FrameQueue final : public IFrameQueue {
+public:
+	FrameQueue() = default;
+
+	bool tryPop(VideoFrame &outFrame) override {
+		std::lock_guard lock(mutex_);
+		if(queue_.empty()) return false;
+		outFrame = std::move(queue_.front());
+		queue_.pop();
+		return true;
+	}
+
+	void push(VideoFrame &&frame) override {
+		std::lock_guard lock(mutex_);
+		queue_.push(std::move(frame));
+	}
+
+private:
+	std::queue<VideoFrame> queue_;
+	std::mutex mutex_;
+};
+
+using frame_queue_t = std::shared_ptr<IFrameQueue>;
+
+#endif //FRAMEQUEUE_HPP
