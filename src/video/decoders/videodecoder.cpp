f517eb0411b25b2519c49cf94b42f4ca1105b5eb
darilin98 <darekrudis@email.cz>
2025-11-21 15:46:06 +0100

    Lay out core members for ffmpeg pipeline

--- src/video/decoders/videodecoder.cpp
+++ src/video/decoders/videodecoder.cpp
@@ -3,3 +3,41 @@
 //
 
 #include "videodecoder.hpp"
+
+VideoDecoder::VideoDecoder(const std::string& extHint) {
+    avformat_network_init();
+}
+
+VideoDecoder::~VideoDecoder() {
+    cleanup();
+    avformat_network_deinit();
+}
+
+void VideoDecoder::cleanup() {
+    if (sws_ctx_) {
+        sws_freeContext(sws_ctx_);
+        sws_ctx_ = nullptr;
+    }
+    if (codec_ctx_) {
+        avcodec_free_context(&codec_ctx_);
+        codec_ctx_ = nullptr;
+    }
+    if (fmt_ctx_) {
+        avformat_close_input(&fmt_ctx_);
+        fmt_ctx_ = nullptr;
+    }
+    if (frame_) {
+        av_frame_free(&frame_);
+        frame_ = nullptr;
+    }
+    if (pkt_) {
+        av_packet_free(&pkt_);
+        pkt_ = nullptr;
+    }
+    video_stream_ = nullptr;
+    video_stream_index_ = -1;
+    reached_eof_ = false;
+    fps_ = 0.0;
+    time_base_seconds_ = 0.0;
+    dst_buf_.clear();
+}
--- src/video/decoders/videodecoder.hpp
+++ src/video/decoders/videodecoder.hpp
@@ -7,13 +7,39 @@
 
 #include "../medialoader.hpp"
 
+extern "C" {
+#include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+#include <libswscale/swscale.h>
+#include <libavutil/imgutils.h>
+#include <libavutil/avutil.h>
+}
 
 class VideoDecoder final : public IMediaDecoder {
 public:
+	explicit VideoDecoder(const std::string& extHint = "");
+	~VideoDecoder() override;
 	bool seekTo(int64_t time) override;
 	bool open(const std::string& path) override;
 	bool decodeNext(VideoFrame& outFrame) override;
 	[[nodiscard]] double frameRate() const override;
+private:
+	void cleanup();
+
+	AVFormatContext* fmt_ctx_ = nullptr;
+	AVCodecContext* codec_ctx_ = nullptr;
+	AVStream* video_stream_ = nullptr;
+	int video_stream_index_ = -1;
+	SwsContext* sws_ctx_ = nullptr;
+	AVFrame* frame_ = nullptr;
+	AVPacket* pkt_ = nullptr;
+
+	double time_base_seconds_ = 0.0;
+	double fps_ = 0.0;
+	bool reached_eof_ = false;
+
+	std::string ext_hint_;
+	std::vector<uint8_t> dst_buf_;
 };
 
 #endif //VIDEODECODER_HPP
