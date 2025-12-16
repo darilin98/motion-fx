f20dc2aa5633be4f024e8866e85eef2b3dabe948
darilin98 <darekrudis@email.cz>
2025-12-16 09:42:55 +0100

    Define IFeatureExtractor

---
+++ src/video/features/ifeatureextractor.hpp
@@ -0,0 +1,21 @@
+//
+// Created by Darek Rudiš on 12.12.2025.
+//
+
+#ifndef IFEATUREEXTRACTOR_HPP
+#define IFEATUREEXTRACTOR_HPP
+
+#include "vsteditcontroller.h"
+#include "../videoframe.hpp"
+#include "base/smartpointer.h"
+
+using econt_t = Steinberg::IPtr<Steinberg::Vst::EditController>;
+class IFeatureExtractor {
+public:
+	virtual ~IFeatureExtractor() = default;
+	virtual void processFrame(const VideoFrame& videoFrame) = 0;
+	virtual void setOutController(const econt_t& controller) = 0;
+};
+
+
+#endif //IFEATUREEXTRACTOR_HPP
